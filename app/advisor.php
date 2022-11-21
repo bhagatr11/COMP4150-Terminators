88c15e7e8d2217cd1efd7a7955e9aef54cb64f75
Joshua Trang <joshua.trang@hotmail.com>
2022-11-21 18:21:27 -0500

    add bootstrap

--- app/advisor.php
+++ app/advisor.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Advisor Menu</title>
+        <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"/>
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous"/>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <?php include('nav.php') ?>
@@ -13,7 +22,7 @@
     include 'dbconfig.php';
 
     $sql = "select * from student";
-    echo '<table border="0" cellspacing="2" cellpadding="2"> 
+    echo '<table class="table table-striped" border="0" cellspacing="2" cellpadding="2"> 
         <tr> 
             <td> <font face="Arial">First Name</font> </td> 
             <td> <font face="Arial">Last Name</font> </td> 
--- app/nav.php
+++ app/nav.php
@@ -1,8 +1,12 @@
-<nav>
-    <h1>COMP 4150</h1>
-    <ul>
-        <a href="advisor.php"><li><span>Advisor Menu</span></li></a>
-        <a href="student.php"><li><span>Student Menu</span></li></a>
-        <a href="staff.php"><li><span>Staff Menu</span></li></a>
-    </ul>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/app">COMP 4150</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="advisor.php"><span>Advisor Menu</span></a></li>
+            <li><a href="student.php"><span>Student Menu</span></a></li>
+            <li><a href="staff.php"><span>Staff Menu</span></a></li>
+        </ul>
+    </div>
 </nav>
